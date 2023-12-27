1cf81f5885036d1b462de5517eb0111a36f8e9ac
nataliia.kravchenko@raiffeisen.ua <nataliia.kravchenko@raiffeisen.ua>
2023-12-27 21:51:56 +0200

    add alerting

--- src/main/java/com/example/springbootweb/controller/PersonController.java
+++ src/main/java/com/example/springbootweb/controller/PersonController.java
@@ -1,12 +1,16 @@
 package com.example.springbootweb.controller;
 
 import com.example.springbootweb.model.Person;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
+    @Counted("person_get_clients_data_count")
+    @Timed("person_get_clients_data_time")
     @GetMapping("/clients")
     public Person getPerson() {
         Person person = new Person();
